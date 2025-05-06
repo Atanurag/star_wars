8bf9bb9004096f2a8a505892c95c7d723b8a794e
Anurag <anurag@iotronsys.com>
2025-05-06 18:22:44 +0530

    star wars

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// firebase config ⚠️
 const firebaseConfig = {
   apiKey: "AIzaSyDub1g_oWvIiAMpgFwUFWKckl0KfIJJ4ME",
   authDomain: "workstation-79b50.firebaseapp.com",
@@ -15,7 +12,6 @@ const firebaseConfig = {
   appId: "1:201443933949:web:f4e2f254b5f22522bdc894"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
